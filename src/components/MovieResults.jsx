29f6b02d3047ae1ecadea2123b05e5d6727708ca
Adam Marsala <adammarsala@hotmail.com>
2021-04-29 14:49:31 -0400

    Write failing tests for MovieResults component

--- src/components/MovieResults.jsx
+++ src/components/MovieResults.jsx
@@ -1,8 +1,17 @@
 export default function MovieResults(props) {
 
+  
+  
+  if (Array.isArray(props.movieList)) {
+    console.log("valid input")
+  } else {
+    console.log("invalid input")
+  }
+
+
   return (
     <section>
-      Movie Results
+      
     </section>
   )
 }
---
+++ src/components/__tests__/MovieResults.test.js
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react'
+import MovieResults from '../MovieResults'
+
+describe('MovieResults basic functionality', () => {
+  it('Shows "no results for" message when given invalid input', () => {
+    const result = render(<MovieResults />)
+    const element = result.queryByText("no results for")
+
+    expect(element).toBeInTheDocument()
+  })
+  it('Shows a list of buttons when given a list of movies', () => {
+    const listOfMovies = ["Rambo", "Greese", "Lion King", "Finding Nemo"]
+
+    const { queryByText, queryAllByTestId } = render(<MovieResults movieList={listOfMovies} />)
+
+    const heading = queryByText("Movie Results")
+    expect(heading).toBeInTheDocument()
+
+    const nominateBtns = queryAllByTestId("nominateBtn")
+
+    expect(nominateBtns.length).toBe(4);
+
+    for (const movie of listOfMovies) {
+      const movieTitle = queryByText(movie)
+      expect(movieTitle).toBeInTheDocument()
+    }
+  })
+})
