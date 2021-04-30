8b7b692d94e4c3e53b1f766473a88fe1db2e3bf9
Adam Marsala <adammarsala@hotmail.com>
2021-04-30 14:34:28 -0400

    Add test for entering input into MovieForm, which is reflected by input value and state

--- src/components/MovieForm.jsx
+++ src/components/MovieForm.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 export default function MovieForm(props) {
   const [movie, setMovie] = useState("");
--- src/components/__tests__/MovieForm.test.js
+++ src/components/__tests__/MovieForm.test.js
@@ -1,3 +1,16 @@
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import MovieForm from '../MovieForm'
 
+describe('MovieForm basic functionality', () => {
+  it('Allows a user to enter a movie as input which is reflected by the state', () => {
+    const result = render(<MovieForm />)
+    const { getByPlaceholderText } = result
+
+    const input = getByPlaceholderText("Enter the movie name")
+
+    fireEvent.click(input)
+    fireEvent.change(input, { target: { value: "spiderman"} })
+
+  })
+  
+})
