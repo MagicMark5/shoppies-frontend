87dd37ffefeca2b2d68bf9be99f28852f6763d45
Adam Marsala <adammarsala@hotmail.com>
2021-04-30 14:41:52 -0400

    Add expect statement to it block for MovieForm test

--- src/components/__tests__/MovieForm.test.js
+++ src/components/__tests__/MovieForm.test.js
@@ -2,15 +2,20 @@ import { render, fireEvent } from '@testing-library/react'
 import MovieForm from '../MovieForm'
 
 describe('MovieForm basic functionality', () => {
+
   it('Allows a user to enter a movie as input which is reflected by the state', () => {
     const result = render(<MovieForm />)
-    const { getByPlaceholderText } = result
+    const { queryByDisplayValue, getByPlaceholderText } = result
 
     const input = getByPlaceholderText("Enter the movie name")
 
     fireEvent.click(input)
     fireEvent.change(input, { target: { value: "spiderman"} })
 
+    const spiderman = queryByDisplayValue("spiderman")
+
+    expect(spiderman).toBeInTheDocument()
   })
   
+
 })
