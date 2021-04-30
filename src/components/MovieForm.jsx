64b337142d60c87ea10decbaca422adbd94170ea
Adam Marsala <adammarsala@hotmail.com>
2021-04-30 15:22:46 -0400

    Add test for clearing input after search button is clicked

--- src/components/MovieForm.jsx
+++ src/components/MovieForm.jsx
@@ -5,14 +5,18 @@ export default function MovieForm(props) {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log(movie);
     // make call to server with movie string (sanitize input?)
     // or pass submission to App 
     // reset input
     setMovie("")
   };
 
-  const handleChange = event => setMovie(event.target.value)
+  const handleChange = event => {
+    const value = event.target.value; 
+    if (value.length <= 50) {
+      setMovie(value)
+    }
+  }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -22,7 +26,7 @@ export default function MovieForm(props) {
         value={movie}
         onChange={handleChange}>
       </input>
-      <button type="submit">Search</button>
+      <button type="submit" data-testid="search">Search</button>
     </form>
   )
 }
--- src/components/__tests__/MovieForm.test.js
+++ src/components/__tests__/MovieForm.test.js
@@ -3,18 +3,39 @@ import MovieForm from '../MovieForm'
 
 describe('MovieForm basic functionality', () => {
 
-  it('Allows a user to enter a movie as input which is reflected by the state', () => {
+  it('Allows a user to enter a movie as input', () => {
+    // Setup: render component and get input inside form
     const result = render(<MovieForm />)
     const { queryByDisplayValue, getByPlaceholderText } = result
-
     const input = getByPlaceholderText("Enter the movie name")
-
+    // Action: click input to select the element and fire change event to change value
     fireEvent.click(input)
     fireEvent.change(input, { target: { value: "spiderman"} })
-
+    // Evaluate and expect result 
     const spiderman = queryByDisplayValue("spiderman")
-
     expect(spiderman).toBeInTheDocument()
+  })
+
+  it('Resets/clears the value of the input after Search button is clicked', () => {
+    const result = render(<MovieForm />)
+    const { queryByDisplayValue, getByPlaceholderText, getByTestId } = result
+    const input = getByPlaceholderText("Enter the movie name")
+    const button = getByTestId("search")
+    // Action: enter input and click Search button
+    fireEvent.click(input)
+    fireEvent.change(input, { 
+        target: { 
+          value: "Lala Land"
+        } 
+      })
+    const lalaLand = queryByDisplayValue("Lala Land")
+    expect(lalaLand).toBeInTheDocument()
+    // click search and submit form 
+    fireEvent.click(button)
+    const lalaLandAfterSearch = queryByDisplayValue("Lala Land")
+    // Evaluate and expect results
+    expect(lalaLandAfterSearch).not.toBeInTheDocument()
+    expect(input.value.length).toBe(0)
   })
   
 
