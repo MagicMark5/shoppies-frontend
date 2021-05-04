33e46b92c241805d928f01e6893375200f229333
Adam Marsala <adammarsala@hotmail.com>
2021-05-04 15:43:58 -0400

    Move makeStyle into styles folder and module and change button colour conditionally

--- src/components/MovieResults.jsx
+++ src/components/MovieResults.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import makeStyles from "../styles/movieResultsStyle";
+import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
@@ -7,58 +7,52 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
 import Icon from '@material-ui/core/Icon';
-//import DeleteIcon from '@material-ui/icons/Delete';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    maxWidth: 752,
-  },
-  demo: {
-    backgroundColor: "inherit"
-  },
-  title: {
-    margin: theme.spacing(4, 0, 2),
-  },
-}));
+
 
 export default function MovieResults(props) {
-  const classes = useStyles();
+  const classes = makeStyles();
   const { movieList, currentNominations, handleNominateAction } = props
   const fiveSelected = currentNominations.length === 5 ? true : false;
   
+  
   const validatedMovies = Array.isArray(movieList) ? movieList : [];
 
-  const generate = (element) => {
-    return validatedMovies.map((movie) =>
-      <ListItem key={`${movie.Title} (${movie.Year})`}>
-        <ListItemAvatar>
-          <Avatar>
-            <Icon className="fas fa-film" />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary={`${movie.Title} (${movie.Year})`}/>
-        <ListItemSecondaryAction>
-          <IconButton 
-            edge="end" 
-            aria-label="add"
-            disabled={fiveSelected || currentNominations.includes(`${movie.Title} (${movie.Year})`) ? true : false}
-            name={`${movie.Title} (${movie.Year})`} 
-            data-testid="nominateBtn" 
-            onClick={nominateMovie}>
-            <Icon className="fas fa-trophy" />
-          </IconButton>
-        </ListItemSecondaryAction>
-      </ListItem>
-    );
-  }
-  
+  // The onClick handler for each nominate movie IconButton 
   const nominateMovie = (event) => {
     const movie = event.target.offsetParent.name;
     handleNominateAction([...currentNominations, movie]);
   }
+
+  // parse movies and conditionally enable buttons
+  const parseMovies = () => {
+    return validatedMovies.map((movie) => {
+      const disableBtn = fiveSelected || currentNominations.includes(`${movie.Title} (${movie.Year})`) ? true : false;
+      const buttonColour = disableBtn ? "default" : "primary";
+      return <ListItem key={`${movie.Title} (${movie.Year})`}>
+              <ListItemAvatar>
+                <Avatar>
+                  <Icon className="fas fa-film" />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={`${movie.Title} (${movie.Year})`}/>
+              <ListItemSecondaryAction>
+                <IconButton 
+                  edge="end" 
+                  aria-label="add"
+                  disabled={disableBtn}
+                  name={`${movie.Title} (${movie.Year})`} 
+                  data-testid="nominateBtn" 
+                  onClick={nominateMovie}>
+                  <Icon className="fas fa-trophy" color={buttonColour} />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+    });
+  }
+  
+
   
   return (
     <section className="results">
@@ -66,7 +60,7 @@ export default function MovieResults(props) {
       {validatedMovies.length > 0 && <Typography variant="h5" className={classes.title}>Movie Results for "{props.query}"</Typography>}
       <div className={classes.demo}>
         <List>
-          {generate()}
+          {parseMovies()}
         </List>
       </div>
     </section>
