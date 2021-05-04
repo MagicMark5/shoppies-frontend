6b34309d412114ebe3847d461de5530edbcc786c
Adam Marsala <adammarsala@hotmail.com>
2021-05-04 15:00:17 -0400

    Add material ui style to movie results, refactor

--- src/components/MovieResults.jsx
+++ src/components/MovieResults.jsx
@@ -1,34 +1,74 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Icon from '@material-ui/core/Icon';
+//import DeleteIcon from '@material-ui/icons/Delete';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 752,
+  },
+  demo: {
+    backgroundColor: "inherit"
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2),
+  },
+}));
+
 export default function MovieResults(props) {
+  const classes = useStyles();
   const { movieList, currentNominations, handleNominateAction } = props
   const fiveSelected = currentNominations.length === 5 ? true : false;
   
   const validatedMovies = Array.isArray(movieList) ? movieList : [];
+
+  const generate = (element) => {
+    return validatedMovies.map((movie) =>
+      <ListItem key={`${movie.Title} (${movie.Year})`}>
+        <ListItemAvatar>
+          <Avatar>
+            <Icon className="fas fa-film" />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary={`${movie.Title} (${movie.Year})`}/>
+        <ListItemSecondaryAction>
+          <IconButton 
+            edge="end" 
+            aria-label="add"
+            disabled={fiveSelected || currentNominations.includes(`${movie.Title} (${movie.Year})`) ? true : false}
+            name={`${movie.Title} (${movie.Year})`} 
+            data-testid="nominateBtn" 
+            onClick={nominateMovie}>
+            <Icon className="fas fa-trophy" />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    );
+  }
   
   const nominateMovie = (event) => {
-    const movie = event.target.name;
+    const movie = event.target.offsetParent.name;
     handleNominateAction([...currentNominations, movie]);
   }
   
-  const parsedMovies = validatedMovies.map(movie => 
-    <li key={`${movie.Title} (${movie.Year})`}>
-      <span>{movie.Title} ({movie.Year})</span>
-      <button 
-        className="fas fa-trophy"
-        disabled={fiveSelected || currentNominations.includes(`${movie.Title} (${movie.Year})`) ? true : false}
-        name={`${movie.Title} (${movie.Year})`} 
-        data-testid="nominateBtn" 
-        onClick={nominateMovie}>
-      </button>
-    </li>
-  )
-  
   return (
     <section className="results">
-      {validatedMovies.length === 0 && <h3>No results for "{props.query}"</h3>}
-      {validatedMovies.length > 0 && <h3>Movie Results</h3>}
-      <ul>
-        {parsedMovies}
-      </ul>
+      {validatedMovies.length === 0 && <Typography variant="h5" className={classes.title}>No results for "{props.query}"</Typography>}
+      {validatedMovies.length > 0 && <Typography variant="h5" className={classes.title}>Movie Results for "{props.query}"</Typography>}
+      <div className={classes.demo}>
+        <List>
+          {generate()}
+        </List>
+      </div>
     </section>
   )
 }
